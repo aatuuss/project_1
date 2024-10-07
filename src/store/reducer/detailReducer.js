0ae25a1f115c8bdd3ae8f640a0d3e5cd649e0cd2
zahra-16 <marjannahzahra@gmail.com>
2024-10-07 17:29:53 +0700

    perminataan perbaikan detail

--- src/store/reducer/detailReducer.js
+++ src/store/reducer/detailReducer.js
@@ -1,15 +1,19 @@
+import { SET_DATA } from "../action/detailAction";
+
 const nilaiDefault = {
-    data: {},
-}
+  data: {},
+};
 
 const detailReducer = (state = nilaiDefault, action) => {
-    switch (action.type) {
-        case SET_DATA:
-            return {
-                ...state,
-                data: action.payload,
-            };
-            default:
-                return state;
-    };
+  switch (action.type) {
+    case SET_DATA:
+      return {
+        ...state,
+        data: action.payload,
+      };
+    default:
+      return state;
+  }
 };
+
+export default detailReducer;
